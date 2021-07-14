4b93a1483de482021f6378dccf43c8c0a8a47873
MZ <matheoz8896@gmail.com>
2021-07-14 23:29:32 +0200

    Returned Api (EUR Only)

--- js_projects/exchange_app/main.js
+++ js_projects/exchange_app/main.js
@@ -7,25 +7,32 @@ const currResult = document.querySelector('#currencyout')
 const rate = document.querySelector('.rate')
 
 
-const check =()=>{
+async function fetchData(){
+    let api_key = `http://data.fixer.io/api/latest?access_key=1af7c3d26a8325bef12a3ffb63645927&format=1`;
+    let response = await fetch(api_key);
+    let data = await response.json();
+    data = JSON.stringify(data);
+    data = JSON.parse(data);
+    return data;
+   }
+
+let ratesInfo = fetchData();
+let exchRate;
+
+//Unfortunately i found only API with BASE EUR, without options
+
+const check = ()=>{
     let from = currInit.value;
     let to = currResult.value;
-    let api_key = `https://api.ratesapi.io/api/latest?base=${from}&symbols=${to}`;
-    
-    fetch(api_key)
-    .then(response => response.json())
-    .then(rates=>{
-        let exchRate;
-        if(from=="EUR"&&to=="EUR"){
-            exchRate = 1;
-        }else{
-            exchRate = rates.rates[to];
-        }
-        rate.textContent = `1 ${from} = ${exchRate.toFixed(4)} ${to}`;
-        cashResult.value = (cashInit.value*exchRate).toFixed(4);
-    })
-    .catch(err => console.error(err));
-}
+    ratesInfo.then(data => { 
+    if(from==to){
+        exchRate = 1;
+    }else{
+        exchRate = data.rates[to];
+    }
+    rate.textContent = `1 ${from} = ${exchRate.toFixed(4)} ${to}`;
+    cashResult.value = (cashInit.value*exchRate).toFixed(4);
+})}
 check();
 
 const replaceValues = () =>{
